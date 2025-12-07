1d603df593ce6b4b6dcc74ad3dc8cd6b52d83d35
ZARMAIN-AHMED <160420162+ZARMAIN-AHMED@users.noreply.github.com>
2025-12-07 14:29:35 +0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ZARMAIN-AHMED.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Physical-AI---Humanoid-Robotics-Course/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'zarinext', // Usually your GitHub org/user name.
-  projectName: 'ai-native-book', // Usually your repo name.
+  organizationName: 'ZARMAIN-AHMED', // Usually your GitHub org/user name.
+  projectName: 'Physical-AI---Humanoid-Robotics-Course', // Usually your repo name.
 
  onBrokenLinks: 'warn',
 onBrokenMarkdownLinks: 'warn',
